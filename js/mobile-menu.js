a98e85960ef612b435bbdd4a52d496214b1bcaf2
in-love-with-anime <vall.bell91@gmail.com>
2021-09-11 17:55:23 +0300

    Дополнила мобменю

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,7 +9,17 @@
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
-      document.body.classList.toggle('modal-open');
+      refs.modal.classList.toggle('is-open')
       refs.modal.classList.toggle('is-hidden');
     }
+  })();
+
+  (() => {
+    const menuNavLink = document.querySelector('[data-menu-link]');
+    const menuBtnRef = document.querySelector('[data-menu-close]');
+    const mobileMenuRef = document.querySelector('[data-menu]');
+    menuNavLink.addEventListener('click', () => {
+      mobileMenuRef.classList.toggle('is-hidden');
+      menuBtnRef.classList.toggle('is-open');
+    });
   })();
