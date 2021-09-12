75339bd559fa4a062c1cf73131248d0faa32bb57
Yurii Havdan <igavdan@gmail.com>
2021-09-12 16:51:17 +0300

    bugfix-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,25 +1,18 @@
 (() => {
     const refs = {
+      modal: document.querySelector('[data-modal]'),
       openModalBtn: document.querySelector('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
+      mobileMenu: document.querySelector('[data-menu-links]'),
     };
-  
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-open')
+        refs.modal.classList.toggle('is-hidden');
+    }
+
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-open')
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
 
-  (() => {
-    const menuNavLink = document.querySelector('[data-menu-link]');
-    const menuBtnRef = document.querySelector('[data-menu-close]');
-    const mobileMenuRef = document.querySelector('[data-menu]');
-    menuNavLink.addEventListener('click', () => {
-      mobileMenuRef.classList.toggle('is-hidden');
-      menuBtnRef.classList.toggle('is-open');
-    });
-  })();
+    refs.mobileMenu.addEventListener('click', toggleModal);
+})();
